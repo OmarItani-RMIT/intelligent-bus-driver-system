0eb1484fafffc126792d75ae665f19a4c1f59611
OmarItani-RMIT <s4096479@student.rmit.edu.au>
2026-05-26 14:59:31 +1000

    feat: link validation to constructor and fix index crash in fromFileString

--- src/main/java/com/busdriver/Driver.java
+++ src/main/java/com/busdriver/Driver.java
@@ -1,5 +1,7 @@
 package com.busdriver;
 
+import com.busdriver.validator.DriverValidator;
+
 /**
  * Driver class represents a bus driver in the Intelligent Bus Driver Guidance System.
  * Each driver has a unique ID, name, experience level, license type, address, and birthdate.
@@ -11,8 +13,6 @@ package com.busdriver;
  *   D3: Birthdate must follow format: DD-MM-YYYY
  *   D5: driverID and name are immutable after creation 
  */
-//TODO: Figure out what constitutes IMMUTABLE
-
 public class Driver {
 
     private String driverID;
@@ -22,33 +22,25 @@ public class Driver {
     private String address;
     private String birthdate;
 
-
     /**
      * Constructor for the Driver Class
-     * @param driverID ID of the driver must be exactly 10 chars, first 2 digits 2-9, at least 2 special chars in positions 3-8, last 2 uppercase letters (A-Z)
-     * @param name name of the driver
-     * @param experienceYears the ammount of experience the driver has in years
-     * @param licenseType the type of liscence they currently hold: Light, Medium, Heavy, PublicTransport
-     * @param address current address where the driver lives: must follow format: Street Number|Street Name|City|State|Country
-     * @param birthdate the birthdate of the driver in the following format: DD-MM-YYYY
      */
     public Driver(String driverID, String name, int experienceYears,
                   String licenseType, String address, String birthdate) {
         
-            // Assign all parameters to fields after validating parameters
-            this.driverID = driverID;
-            this.name = name;
-            this.experienceYears = experienceYears;
-            this.licenseType = licenseType;
-            this.address = address;
-            this.birthdate = birthdate;
+        this.driverID = driverID;
+        this.name = name;
+        this.experienceYears = experienceYears;
+        this.licenseType = licenseType;
+        this.address = address;
+        this.birthdate = birthdate;
         
-
-        
+        // Omar's Task: Trigger validation upon creation (D1, D2, D3)
+        // This makes sure invalid drivers are rejected immediately
+        DriverValidator.validateDriver(this);
     }
 
-
-    // GETTERS:
+    // GETTERS
     public String getDriverID() { return this.driverID; }
     public String getName() { return this.name; }
     public int getExperienceYears() { return this.experienceYears; }
@@ -56,54 +48,55 @@ public class Driver {
     public String getAddress() { return this.address; }
     public String getBirthdate() { return this.birthdate; }
 
-
-
-    //SETTERS: (note: NO setter for driverID and name - they are immutable per D5)
+    // SETTERS (Note: No setters for driverID and name to enforce D5 immutability)
     public void setExperienceYears(int experienceYears) { this.experienceYears = experienceYears; }
     public void setLicenseType(String licenseType) { this.licenseType = licenseType; }
-
-    public void setAddress(String address) { this.address = address; } //one line if statements are so fancy >u>
+    public void setAddress(String address) { this.address = address; } 
     public void setBirthdate(String birthdate) { this.birthdate = birthdate; }
 
-
-
-
-
-
-    // TODO: Implement toFileString() - converts Driver to pipe-delimited string for TXT storage
-    // Format: driverID|name|experienceYears|licenseType|StreetNum|StreetName|City|State|Country|birthdate
+    /**
+     * Converts Driver to pipe-delimited string for TXT storage
+     * Format: driverID|name|experienceYears|licenseType|StreetNum|StreetName|City|State|Country|birthdate
+     */
     public String toFileString() { 
-        String returnString = "";
+        return driverID + "|" + name + "|" + experienceYears + "|" + licenseType + "|" + address + "|" + birthdate;
+    }
 
-        returnString = driverID + "|" + name + "|" + experienceYears + "|" + licenseType + "|" + address + "|" + birthdate;
-
-        return returnString;
-     }
-
-    
+    /**
+     * Parses a pipe-delimited line from a TXT file and reconstructs a Driver object
+     */
     public static Driver fromFileString(String line) { 
-        //return variable
-        Driver returnDriver = null;
-
-        //Split based on regex |
+        // Split based on pipe delimiter
         String[] splitLine = line.split("\\|");
 
-        //Store each part in its own variable
-        String driverID = splitLine[0], name = splitLine[1],
-                liscenceType = splitLine[3], StreetNum = splitLine[4], StreetName = splitLine[5],
-                City = splitLine[6], State = splitLine[7], Country = splitLine[9], birthdate = splitLine[10];
+        // Extract each part using corrected indexing (0 to 9)
+        String driverID = splitLine[0];
+        String name = splitLine[1];
         int experienceYears = Integer.parseInt(splitLine[2]);
+        String licenseType = splitLine[3];
+        String streetNum = splitLine[4];
+        String streetName = splitLine[5];
+        String city = splitLine[6];
+        String state = splitLine[7];
+        String country = splitLine[8]; // Corrected index
+        String birthdate = splitLine[9]; // Corrected index
 
-        // construct the address part
-        String address = StreetNum + "|" + StreetName + "|" + City + "|" + State + "|" + Country + "|";
+        // Construct the address string without any trailing pipes
+        String address = streetNum + "|" + streetName + "|" + city + "|" + state + "|" + country;
 
-        // create a new driver
-        returnDriver = new Driver(driverID, name, experienceYears, liscenceType, address, birthdate);
-
-        // return the driver
-        return returnDriver;
-     }  
+        // Return the validated, reconstructed driver
+        return new Driver(driverID, name, experienceYears, licenseType, address, birthdate);
+    }  
 
     @Override
-    public String toString() { return "Driver{}"; }
+    public String toString() { 
+        return "Driver{" +
+                "driverID='" + driverID + '\'' +
+                ", name='" + name + '\'' +
+                ", experienceYears=" + experienceYears +
+                ", licenseType='" + licenseType + '\'' +
+                ", address='" + address + '\'' +
+                ", birthdate='" + birthdate + '\'' +
+                '}'; 
+    }
 }
